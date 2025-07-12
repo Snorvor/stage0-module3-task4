b5428577463b07d46f40dbf5725fc0184baa5f61
Snorvor <bulezar@gmail.com>
2025-07-12 15:55:58 -0300

    Formula created

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 97, b = 98;
+
+        int formulaFirstPart = (9 * a * a - 5 * b + 2 + a - 7);
+
+        int formulaSecondPart = (a + b - 4 * a * b) / 2;
+
+        int result = formulaFirstPart * formulaSecondPart;
+
+        System.out.println(result);
     }
 }
